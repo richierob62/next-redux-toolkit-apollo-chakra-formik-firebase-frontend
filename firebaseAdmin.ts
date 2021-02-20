4830aedcca1f3e078eb823a8bab136b88f07cd91
richierob62 <richierob62@gmail.com>
2021-02-19 23:11:41 -0500

    Moved service account object

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,6 +1,17 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 
-import serviceAccount from './auth-secrets';
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+};
 
 export const verifyIdToken = (token: string) => {
   if (!admin.apps.length) {
